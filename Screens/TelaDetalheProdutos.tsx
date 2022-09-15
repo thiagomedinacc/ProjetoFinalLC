20e8af33e7e8d1d120f9021a72743c524feda94d
F9329136 Thiago Pizzio Medina <thiago.medina@bb.com.br>
2022-09-15 13:18:51 -0300

    Inicia tela de detalhe dos produtos

---
+++ Screens/TelaDetalheProdutos.tsx
@@ -0,0 +1,7 @@
+import {Text} from 'react-native' 
+
+export default function TelaDetalheProdutos() {
+    return (
+        <Text>Tela de Detalhes dos produtos </Text>
+    )
+}
--- navigation/Navigate.tsx
+++ navigation/Navigate.tsx
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
 import TelaCarrinho from '../Screens/TelaCarrinho';
 import Home from '../Screens/Home';
+import TelaDetalheProdutos from '../Screens/TelaDetalheProdutos';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -21,6 +22,7 @@ export default function Navigate() {
         options={{title: 'TelaCarrinho'}}
       />
       <Stack.Screen name="Home" component={Home} options={{title: 'Home'}} />
+      <Stack.Screen name="TelaDetalheProdutos" component= {TelaDetalheProdutos} options= {{title: 'Detalhe do produto'}} />
     </Stack.Navigator>
   );
 }
